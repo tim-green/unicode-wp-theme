31974c88894a171f1afc6a496cdcd3d636079f60
Tim <github@timgreen.ws>
2021-03-29 14:24:32 +1100

    framed out feature content block for ACF

---
+++ partials/flexible-layouts/flexible-feature-content-block.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  ACF Flexible Content - Template Name Here
+  
+  Field Example
+  <?php the_sub_field('text'); ?>
+*/
+?>
+
+<div class="uc-feature-story workfront">
+    <div class="hero is-medium">
+        <div class="hero-image">
+            <span class="background-image" role="img" style="background: url('');"></span>
+        </div>
+        <div class="shadow"></div>
+
+        <div class="container">
+            <div class="content">
+                <h2></h2>
+
+                <a href="" class="uc-feature-story__button" rel="noopener noreferrer">
+                    
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+    
