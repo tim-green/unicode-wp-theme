94eee430d89f71b50c1fd184683927dcba2d8361
Tim <github@timgreen.ws>
2021-04-06 02:13:01 +1000

    added breadcrumb

--- index.php
+++ index.php
@@ -27,5 +27,10 @@
 		</div><!-- /.col -->
 		
 	</div><!-- /.row -->
+<?php if(function_exists('bcn_display')) : ?>
+<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">   
+    <?php bcn_display();	?>
+</div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
