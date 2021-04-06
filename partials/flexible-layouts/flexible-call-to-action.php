7e44fa10598ab304a6e3265a9d10ddf6500f908f
Tim <github@timgreen.ws>
2021-04-07 00:37:06 +1000

    added CTA html structure and added ACF field calls

--- partials/flexible-layouts/flexible-call-to-action.php
+++ partials/flexible-layouts/flexible-call-to-action.php
@@ -6,3 +6,11 @@
   <?php the_sub_field('text'); ?>
 */
 ?>
+
+<div class="uc-cta-banner uc-cta-banner-image background-image" style="background-image: url('<?php the_sub_field('image'); ?>')!important;" aria-label="call to action">
+    <div class="container">
+        <a href="<?php the_sub_field('button_link'); ?>" class="uc-cta-banner-image__button" rel="noopener noreferrer">
+            <?php the_sub_field('button_text'); ?>
+        </a>
+    </div>
+</div>
