cd01f764ab2b2bd9f3251e06e3e2534ed76d8d34
Tim <github@timgreen.ws>
2021-04-12 18:45:52 +1000

    started core markup for card block

--- partials/flexible-layouts/flexible-four-card-block.php
+++ partials/flexible-layouts/flexible-four-card-block.php
@@ -6,3 +6,21 @@
   <?php the_sub_field('text'); ?>
 */
 ?>
+<div class="section is-large has-background-grey-lighter">
+    <div class="columns is-multiline">
+        <div class="column">
+            <a href="" class="card media link-card">
+            <div class="image is-2by1 background-image" role="img" style="background-image: url('<?php echo $image_1; ?>')!important;">
+            </div>
+            <div class="media-content">
+                <p class="title is-4"><?php echo $heading_1; ?></p>
+                <p class="has-text-align-center">
+                </p> 
+            </div>
+            </a>
+        </div>
+
+        
+       </div> 
+    </div>
+</div>
