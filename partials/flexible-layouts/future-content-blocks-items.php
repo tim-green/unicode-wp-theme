4d2c3e6589c814fd77db9c080ace8fc02f4c6097
Tim <github@timgreen.ws>
2021-04-14 20:14:40 +1000

    code mockup for future content block for ACF

---
+++ partials/flexible-layouts/future-content-blocks-items.php
@@ -0,0 +1,16 @@
+<div class="content">
+        <div class="columns is-multiline">
+            <div class="column"></div>
+
+            <div class="column is-three-quarters-tablet is-two-thirds-desktop is-two-thirds-widescreen">
+                <h2 class="has-text-align-center">
+                    New Feature Block
+                </h2>
+                
+                <p class="has-text-align-center">
+                Content New Feature Block
+                </p> 
+            </div>
+            <div class="column"></div>
+        </div>
+    </div>
